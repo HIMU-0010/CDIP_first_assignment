19d057a495fd74576fca8ed0f543acd43938dae0
HIMU-0010 <mirazkamalhimel10@gmail.com>
2024-08-19 12:21:14 +0600

    New file (5)Lists.py added

---
+++ (5)Lists.py
@@ -0,0 +1,13 @@
+fruitsList = ['lichi', 'mango', 'orange', 'kiwi', 'blueberry']
+
+#new list using list comprehension
+fruitsLenList = [len(x) for x in fruitsList]
+print(fruitsLenList)
+
+#insert new item ina list
+fruitsList.append('watermelon')
+print(fruitsList)
+
+#sort list
+fruitsList.sort()
+print(fruitsList)
